38359314fe230b32b32366a6e31fe6b9e528343b
Alex <alexzarodaeli8@gmail.com>
2022-04-10 11:14:02 +0700

    First Commit

---
+++ src/personal/financial/records/application/Encap.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personal.financial.records.application;
+
+/**
+ *
+ * @author Alex Zaro Daeli
+ */
+public class Encap {
+    
+}
---
+++ src/personal/financial/records/application/PersonalFinancialRecordsApplication.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package personal.financial.records.application;
+import form.FrameAplication;
+/**
+ *
+ * @author Alex Zaro Daeli
+ */
+public class PersonalFinancialRecordsApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     FrameAplication frame = new FrameAplication();
+     frame.setLocationRelativeTo(null); 
+     frame.setVisible(true);
+    }
+    
+}
